875e32ab7a81fa642b2138db900570d3c36362af
nzumolinda <nzumolinda@yahoo.fr>
2017-04-27 05:02:37 -0400

    map_modified

--- app/src/main/java/com/example/francoisluc/ift2905_projet/MapActivity.java
+++ app/src/main/java/com/example/francoisluc/ift2905_projet/MapActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -35,9 +36,9 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         myMap = googleMap;
-        myMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+        LatLng Montreal = new LatLng(45.528845, -73.565337);
+        myMap.addMarker(new MarkerOptions().position(Montreal).title("Marker in Montreal"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Montreal, 15));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
