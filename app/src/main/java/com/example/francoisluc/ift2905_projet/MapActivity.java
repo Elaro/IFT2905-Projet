6503d41c3dc45fefd29ce9c926bdff2cbcde713c
nzumolinda <nzumolinda@yahoo.fr>
2017-04-27 11:05:28 -0400

    map_modified2

--- app/src/main/java/com/example/francoisluc/ift2905_projet/MapActivity.java
+++ app/src/main/java/com/example/francoisluc/ift2905_projet/MapActivity.java
@@ -25,31 +25,24 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_bixi);
+        setContentView(R.layout.content_docks);
+        setContentView(R.layout.content_itinerary);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         //myMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        //onMapReady(myMap);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        myMap = googleMap;
-        LatLng Montreal = new LatLng(45.528845, -73.565337);
-        myMap.addMarker(new MarkerOptions().position(Montreal).title("Marker in Montreal"));
-        myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Montreal, 15));
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        myMap.setMyLocationEnabled(true);
+    public void onMapReady(GoogleMap myMap) {
+
+        LatLng Montreal = new LatLng(-34, 151);
+        myMap.addMarker(new MarkerOptions().position(Montreal).title("Marker_Montreal"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(Montreal));
+
 
     }
 
